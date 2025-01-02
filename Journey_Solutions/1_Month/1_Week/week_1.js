0536a520fb836dd987b8421f22a431c5d49302c6
aysieelf <aysel.karadayi.a63@learn.telerikacademy.com>
2025-01-02 10:21:17 +0200

    add javascript solution of two sum in week_1.js

---
+++ Journey_Solutions/1_Month/1_Week/week_1.js
@@ -0,0 +1,11 @@
+// 1. Two Sum
+var twoSum = function(nums, target) {
+    const seen = {};
+    for (const [i, num] of nums.entries()) {
+        const diff = target - num
+        if (diff in seen) {
+            return [seen[diff], i]
+        }
+        seen[num] = i
+    }
+};
