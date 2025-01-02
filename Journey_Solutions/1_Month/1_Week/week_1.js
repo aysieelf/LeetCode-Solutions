e03f9909b41bda16f2225f106c12029a7a87512c
aysieelf <aysel.karadayi.a63@learn.telerikacademy.com>
2025-01-02 10:37:35 +0200

    add: solution for 88. Merge Sorted Array in javascript language in week_1.js

--- Journey_Solutions/1_Month/1_Week/week_1.js
+++ Journey_Solutions/1_Month/1_Week/week_1.js
@@ -2,12 +2,28 @@
 var twoSum = function(nums, target) {
     const seen = {};
     for (const [i, num] of nums.entries()) {
-        const diff = target - num
+        const diff = target - num;
         if (diff in seen) {
-            return [seen[diff], i]
+            return [seen[diff], i];
         }
-        seen[num] = i
+        seen[num] = i;
     }
 };
 
-//
+// 88. Merge Sorted Array
+var merge = function(nums1, m, nums2, n) {
+    let last = m + n - 1;
+    let i1 = m - 1;
+    let j2 = n - 1;
+    while (j2 >= 0) {
+        if (i1 < 0 || nums2[j2] > nums1[i1]) {
+            nums1[last] = nums2[j2];
+            j2 -= 1;
+        }
+        else {
+            nums1[last] = nums1[i1];
+            i1 -= 1;
+        }
+        last -= 1;
+    }
+};
