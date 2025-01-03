dae522e06365f5fcc81b329a5d5a261278257533
aysieelf <aysel.karadayi.a63@learn.telerikacademy.com>
2025-01-03 10:43:42 +0200

    add: javascript solutions for 217. Contains Duplicate problem in week_1.js

--- Journey_Solutions/1_Month/1_Week/week_1.js
+++ Journey_Solutions/1_Month/1_Week/week_1.js
@@ -47,3 +47,29 @@ var runningSum = function(nums) {
     }
     return result;
 };
+
+// 217. Contains Duplicate
+var containsDuplicate = function(nums) {
+  const seen = new Set();
+  for (const num of nums) {
+      if (seen.has(num)) {
+          return true;
+      }
+      seen.add(num);
+  }
+  return false;
+};
+
+var containsDuplicate2 = function(nums) {
+    return new Set(nums).size !== nums.length;
+};
+
+var containsDuplicate3 = function (nums) {
+    nums.sort();
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] === nums[i-1]) {
+            return true;
+        }
+    }
+    return false;
+};
