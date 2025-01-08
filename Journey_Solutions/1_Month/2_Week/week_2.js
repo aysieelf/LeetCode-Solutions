10c8d1ed380c8c04b5c5ab1fe27a387d11104aa6
aysieelf <aysel.karadayi.a63@learn.telerikacademy.com>
2025-01-08 10:00:51 +0200

    solve 26_Remove_duplicates_from_sorted_array in JavaScript in week_2.js

--- Journey_Solutions/1_Month/2_Week/week_2.js
+++ Journey_Solutions/1_Month/2_Week/week_2.js
@@ -85,3 +85,19 @@ var plusOne = function(digits) {
         }
     }
 };
+
+// 26. Remove Duplicates from Sorted Array
+var removeDuplicates = function(nums) {
+   if (nums.length === 1) {
+       return 1;
+   }
+
+   let i = 0;
+   for (let j = 1; j < nums.length; j++) {
+       if (nums[i] !== nums[j]) {
+           i++;
+           nums[i] = nums[j];
+       }
+   }
+   return i + 1;
+};
