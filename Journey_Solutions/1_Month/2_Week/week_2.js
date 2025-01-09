8f2f0fc049e7d79ce2067bd848b9051f39b03fc3
aysieelf <aysel.karadayi.a63@learn.telerikacademy.com>
2025-01-09 10:30:31 +0200

    add:  solved 35. Search Insert Position in JavaScript in week_2.js

--- Journey_Solutions/1_Month/2_Week/week_2.js
+++ Journey_Solutions/1_Month/2_Week/week_2.js
@@ -101,3 +101,25 @@ var removeDuplicates = function(nums) {
    }
    return i + 1;
 };
+
+
+// 35. Search Insert Position
+var searchInsert = function(nums, target) {
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left <= right) {
+        const mid = Math.floor((left + right) / 2);
+
+        if (target === nums[mid]) {
+            return mid;
+        }
+        else if (target < nums[mid]) {
+            right = mid - 1;
+        }
+        else {
+            left = mid + 1;
+        }
+    }
+    return left;
+};
