fd63ffe5bc53353bdb53ca9dce8ce0cb5aab27d7
aysieelf <aysel.karadayi.a63@learn.telerikacademy.com>
2025-01-12 12:27:00 +0200

    solve: 27_Remove_Element.py in JavaScript in week_2.js

--- Journey_Solutions/1_Month/2_Week/week_2.js
+++ Journey_Solutions/1_Month/2_Week/week_2.js
@@ -162,3 +162,15 @@ var pivotIndex = function(nums) {
     }
     return -1;
 };
+
+// 27. Remove Element
+var removeElement = function(nums, val) {
+    let k = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== val) {
+            nums[k] = nums[i];
+            k += 1;
+        }
+    }
+    return k;
+};
