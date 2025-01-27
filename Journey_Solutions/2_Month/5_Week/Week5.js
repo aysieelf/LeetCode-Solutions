c72d81cf25ca270496f22d871daae76b7a9770a5
aysieelf <aysel.karadayi.a63@learn.telerikacademy.com>
2025-01-27 12:30:15 +0200

    solve: 389. Find the Difference in JS

---
+++ Journey_Solutions/2_Month/5_Week/Week5.js
@@ -0,0 +1,14 @@
+// 389. Find the Difference
+var findTheDifference = function(s, t) {
+    let result = 0;
+
+    for (const char of s) {
+        result ^= char.charCodeAt(0);
+    }
+
+    for (const char of t) {
+        result ^= char.charCodeAt(0);
+    }
+
+    return String.fromCharCode(result);
+};
